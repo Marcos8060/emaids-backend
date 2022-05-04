edfbd367b06ede14c996d2d8d6be8f8857f8e0ca
Marcos8060 <marcosgav80@gmail.com>
2022-05-04 10:48:41 +0300

    changed the allowed origins url

--- maids/settings.py
+++ maids/settings.py
@@ -201,7 +201,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:3000",
+    "https://nairobi-maids.netlify.app/",
 ]
 
 SIMPLE_JWT = {
